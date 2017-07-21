638618896a1d23b56146fb3565b87759d6433f84
mbstuart <mattburnstu@gmail.com>
2017-07-21 23:29:25 +0100

    Creating slider component

--- src/app/slider/slider.module.ts
+++ src/app/slider/slider.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SliderComponent } from './src/app/slider/slider/slider.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [SliderComponent]
 })
 export class SliderModule { }
